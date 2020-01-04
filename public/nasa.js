beb7f4591a5934f93d07c46c0242845e0bc320af
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-04 00:03:30 -0500

    deleted some Date obj handling and cleaned up code

--- public/nasa.js
+++ public/nasa.js
@@ -1,34 +1,21 @@
 document.getElementById('search').addEventListener('click', search_function);
 
-document.getElementById('prevday').addEventListener('click', previousday_function);
-
-document.getElementById('nxtday').addEventListener('click', nextday_function);
-
     let width = this.innerWidth, //this refers to window
-        height = this.innerHeight,
-    //this will be for getting the current date onload
-        computerDate = new Date(),
-        dd = String(computerDate.getDate()).padStart(2, '0'),
-        mm = String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
-        yyyy = computerDate.getFullYear(),
-        today = yyyy + '-' + mm + '-' + dd;
+        height = this.innerHeight;
 
-    console.log(today);
-
-call_api_function()
 function call_api_function() {
 
     let xhr = new XMLHttpRequest() , endpoint = `https://api.nasa.gov/planetary/apod`, myKey = `?api_key=Fgrr1lJp3BQ5AUgM9k0EuSkLS0R9RhUKbSavz4dP`;
 
     endpoint += myKey + "&date=" + today + '&hd=true';
     
-    console.log(endpoint);
+    // console.log(endpoint);
 
     xhr.open('GET', endpoint ,true);
 
     xhr.onload = () => {
 
-        console.log(JSON.parse(xhr.responseText));
+        // console.log(JSON.parse(xhr.responseText));
         
         const nasaData = JSON.parse(xhr.responseText);
 
@@ -57,26 +44,4 @@ function search_function() {
     call_api_function()
 }
 
-function previousday_function() {
-
-    // if (document.getElementById('dateinput').value == "") {
-    //     today = yyyy + '-' + mm + '-' + (parseInt(dd) - 1);
-    // } else {
-    //     today = document.getElementById('dateinput').value;
-    // }
-
-    // if
-
-    call_api_function()
-}
-
-function nextday_function() {
-    if (document.getElementById('dateinput').value == "") {
-        today = yyyy + '-' + mm + '-' + (parseInt(dd) + 1);
-    } else {
-        today = document.getElementById('dateinput').value;
-    }
-
-    call_api_function()
-}
 
